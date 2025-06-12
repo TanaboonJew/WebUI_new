aa848658707ce511502cd953a665267bddd359c6
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-06-12 16:26:30 +0700

    fix ai dashborad V1.17

--- core/views.py
+++ core/views.py
@@ -318,7 +318,6 @@ def ai_dashboard(request):
                     messages.success(request, "Model uploaded successfully")
                     return redirect('ai-dashboard')
 
-    print(containers.status)
     print("containers:", containers)
     print("jupyter_url:", jupyter_url)
     print("jupyter_token:", jupyter_token)
