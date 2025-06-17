d5c86b2d923b99b904bb5f78088610b025b6e7f0
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-06-17 17:19:58 +0700

    test docker auto stop container

--- core/tasks.py
+++ core/tasks.py
@@ -1,5 +1,5 @@
 from background_task import background
-from core.docker_util import DockerManager
+from core.docker_utils import DockerManager
 
 @background(schedule=600)  # รันครั้งแรกหลัง 10 นาที (600 วินาที)
 def cleanup_containers_task():
