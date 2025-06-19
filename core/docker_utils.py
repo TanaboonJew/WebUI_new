b3fd37377c5cc9f2986f7a64e4d6d6554edf9dcc
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-06-19 12:35:52 +0700

    edit jupyter

--- core/docker_utils.py
+++ core/docker_utils.py
@@ -131,6 +131,7 @@ class DockerManager:
                         'JUPYTER_TOKEN': token,
                         'GRANT_SUDO': 'yes'
                     },
+                    command="start-notebook.sh --NotebookApp.notebook_dir='/home/user/work'",
                     detach=True,
                     mem_limit=f"{user.mem_limit}m",
                     memswap_limit=f"{user.memswap_limit}m",
