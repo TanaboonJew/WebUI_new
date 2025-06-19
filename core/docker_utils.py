88d6fb81e4f5760a95066240bf42a10e6381b71d
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-06-19 14:27:23 +0700

    fix jupyter

--- core/docker_utils.py
+++ core/docker_utils.py
@@ -131,7 +131,6 @@ class DockerManager:
                         'JUPYTER_TOKEN': token,
                         'GRANT_SUDO': 'yes'
                     },
-                    command="jupyter notebook --NotebookApp.notebook_dir='/home/user' --ip=0.0.0.0 --port=8888 --no-browser",
                     detach=True,
                     mem_limit=f"{user.mem_limit}m",
                     memswap_limit=f"{user.memswap_limit}m",
@@ -155,7 +154,7 @@ class DockerManager:
                     }
                 )
 
-                return f"http://{settings.SERVER_IP}:{port}/tree/home/user?token={token}", token
+                return f"http://{settings.SERVER_IP}:{port}/?token={token}", token
 
             else:
                 pass  # สำหรับ container ประเภทอื่น
