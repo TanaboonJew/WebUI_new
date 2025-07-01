87ffb913957e81817f233d6fa30f72c634c1b9ac
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-07-01 13:39:48 +0700

    Schedule V1.2

--- core/apps.py
+++ core/apps.py
@@ -1,17 +1,18 @@
 from django.apps import AppConfig
-from apscheduler.schedulers.background import BackgroundScheduler
-from django_apscheduler.jobstores import DjangoJobStore
-import atexit
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
+        import atexit
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from django_apscheduler.jobstores import DjangoJobStore
+        from .jobs import schedule_all_containers
+
         scheduler = BackgroundScheduler()
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
-        from .jobs import schedule_all_containers  
         schedule_all_containers(scheduler)
 
         scheduler.start()
