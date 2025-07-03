88f75beae8c9d9379437c4db1ea3c64b3e4573cb
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-07-03 12:18:20 +0700

    edit manage.py

--- manage.py
+++ manage.py
@@ -8,21 +8,23 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebUI.settings')
 
-    import django
-    django.setup() 
-
-    from core.scheduler import start_scheduler
-    start_scheduler()
-
     try:
         from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+
+    # ✅ เรียก start_scheduler เฉพาะเมื่อใช้ runserver เท่านั้น
+    if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+        import django
+        django.setup()
+
+        from core.scheduler import start_scheduler
+        start_scheduler()
 
 
 if __name__ == '__main__':
